f626cb306ca0866007f20d6e40bdc88d2ced41f1
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-05-12 19:19:19 -0300

    final touches

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import submit from './modules/submit.js';
 
 if (localStorage.getItem('gameid') === null) {
   createGame('Game2');
+} else {
+  refresh();
 }
 
 const refreshBtn = document.getElementById('Refresh-btn');
 const submitBtn = document.getElementById('Submit-btn');
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+
+const submitOnEnter = (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    submitBtn.click();
+  }
+};
 
 refreshBtn.addEventListener('click', refresh);
 submitBtn.addEventListener('click', submit);
+nameInput.addEventListener('keypress', submitOnEnter);
+scoreInput.addEventListener('keypress', submitOnEnter);
